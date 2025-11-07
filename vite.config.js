c742eb0dafef173c8d7c7c4d8c20e85ff36ff824
ollie <oleg.naumov@grunitech.com>
2025-11-07 20:06:19 +0000

    Final configuration fix using --base=./ in build script

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Установка базового пути для GitHub Pages.
-// Это необходимо, так как приложение размещено в подпапке /log-parser-app/
 export default defineConfig({
-  // Замените '/log-parser-app/' на имя вашего репозитория, если оно другое.
-  base: '/log-parser-app/', 
   plugins: [react()],
-  // Vite 7.x uses 'optimizeDeps' for dependencies
   optimizeDeps: {
     include: ['react', 'react-dom'],
   }
-}
+})
